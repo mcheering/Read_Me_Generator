b92519c1fd035066249ab66a0ec57b5f91f39f32
mcheering <62622009+mcheering@users.noreply.github.com>
2021-01-17 16:35:11 -0600

    updated js file to now generate a readme file with formatting. Needs more polish still, such as, some things are on the same line, need to add breaks to separate lines.

--- Index.js
+++ Index.js
@@ -17,24 +17,53 @@ inquirer
                   type: 'list',
                   message: 'Select a license for your application.',
                   name: 'license',
-                  choices: ["MIT", "Ceative Common Licesne Family", "Eclipse Public License", "GNU v3.0"]
+                  choices: ["MIT", "Ceative Common Licesne Family", "Eclipse Public License", "GNU v3.0"],
             },
             {
                   type: 'input',
                   message: "Enter yout github username",
-                  name: 'license',
+                  name: 'username',
             },
             {
                   type: 'input',
                   message: 'enter your email address',
                   name: 'email',
-            }
+            },
       ])
       .then((response) => {
             console.log(response);
-            let stringResponse = JSON.stringify(response)
-            fs.appendFile('log.txt', `${stringResponse}\n`, (err) =>
-                  err ? console.error(err) : console.log('Commit logged!')
+            console.log(response.title)
+
+            switch (response.license) {
+                  case "MIT":
+                        imageAddress = "https://img.shields.io/badge/License-MIT-yellow.svg)";
+                        break;
+                  case "Creative Commons License Family":
+                        imageAddress = "https://licensebuttons.net/l/zero/1.0/80x15.png";
+                        break;
+                  case "Eclipse Public License":
+                        imageAddress = "https://img.shields.io/badge/License-EPL%201.0-red.svg";
+                        break;
+                  case "GNU v3.0":
+                        imageAddress = "https://img.shields.io/badge/License-GPLv3-blue.svg";
+                        break;
+            }
+
+            fs.appendFile('Generated_README.md',
+                  `#${response.title}
+                  <br>
+                  ##Description
+                  ${response.description}
+                  <br>
+                  ##License
+                  ![${response.license} image](${imageAddress})
+                  This application use the ${response.license}  license.  
+                  <br>
+                  ##Questions
+                  Below are two methods you can contact me about any questions you have, ideas, or issues you encounter using the application. The first link is to my github where you can post your question.  The other is my email where you can contact me directly. 
+                  -[Github](https://github.com/${response.username})
+                  -[email](${response.email})`,
+                  (err) => err ? console.error(err) : console.log('Commit logged!')
             );
       }
-      );
+      ); 
